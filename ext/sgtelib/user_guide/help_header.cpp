9e52400424562ae79ad352681854e30ecee7a5bc
Viviane Rochon Montplaisir <viviane.rochon.montplaisir@gerad.ca>
2020-11-30 10:45:35 -0500

    Update sgtelib

--- ext/sgtelib/user_guide/help_header.cpp
+++ ext/sgtelib/user_guide/help_header.cpp
@@ -2,7 +2,7 @@
 /*  sgtelib - A surrogate model library for derivative-free optimization               */
 /*  Version 2.0.2                                                                      */
 /*                                                                                     */
-/*  Copyright (C) 2012-2017  Sebastien Le Digabel - Ecole Polytechnique, Montreal      */ 
+/*  Copyright (C) 2012-2016  Sebastien Le Digabel - Ecole Polytechnique, Montreal      */ 
 /*                           Bastien Talgorn - McGill University, Montreal             */
 /*                                                                                     */
 /*  Author: Bastien Talgorn                                                            */
@@ -23,63 +23,7 @@
 /*  You can find information on sgtelib at https://github.com/bastientalgorn/sgtelib   */
 /*-------------------------------------------------------------------------------------*/
 
-#ifndef __SGTELIB_SURROGATE_PRS__
-#define __SGTELIB_SURROGATE_PRS__
 
-#include "Surrogate.hpp"
-
-namespace SGTELIB {
-
-  /*--------------------------------------*/
-  /*         Surrogate_PRS class        */
-  /*--------------------------------------*/
-  class Surrogate_PRS : public SGTELIB::Surrogate {
-
-    /*--------------------------------------------------------*/
-    /*  these members are defined in the Surrogate superclass */
-    /*--------------------------------------------------------*/
-    // int _p; // number of data points in X and Z
-    // int _n; // dimension -- number of variables
-    // int _m; // number of outputs (includes the objective)
-
-  protected:
-
-    int _q; // Nb of basis function
-    SGTELIB::Matrix _M; // Monomes
-    SGTELIB::Matrix _H; // Design matrix
-    SGTELIB::Matrix _Ai; // Inverse of Ht*H
-    SGTELIB::Matrix _alpha; // Coefficients
-
-    virtual const SGTELIB::Matrix compute_design_matrix ( const SGTELIB::Matrix& Monomes, 
-                                                          const SGTELIB::Matrix & Xs );
-
-    // build model (private):
-    virtual bool build_private (void) override;
-
-    void predict_private ( const SGTELIB::Matrix & XXs,
-                                 SGTELIB::Matrix * ZZs) override;
+#include "sgtelib_help.hpp"
 
 
-    // Compute metrics
-    const SGTELIB::Matrix * get_matrix_Zvs (void) override;
-
-    bool compute_alpha ( void );
-
-  public:
-
-    // Constructor
-    Surrogate_PRS ( SGTELIB::TrainingSet & trainingset ,   
-                    SGTELIB::Surrogate_Parameters param) ;
-
-    // destructor:
-    virtual ~Surrogate_PRS ( void );
-
-    // Build the monome exponents
-    static int get_nb_PRS_monomes(const int nvar, const int degree);
-    static SGTELIB::Matrix get_PRS_monomes(const int nvar, const int degree);
-    virtual void display_private ( std::ostream & out ) const override;
-
-  };
-}
-
-#endif
