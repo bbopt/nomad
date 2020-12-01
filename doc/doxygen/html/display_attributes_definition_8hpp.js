4ba6030a48725d5c2feff1340e874cc8688d92e7
Viviane Rochon Montplaisir <viviane.rochon.montplaisir@gerad.ca>
2020-12-01 17:09:10 -0500

    Update doxygen

--- doc/doxygen/html/display_attributes_definition_8hpp.js
+++ doc/doxygen/html/display_attributes_definition_8hpp.js
@@ -1,5 +1,4 @@
 var display_attributes_definition_8hpp =
 [
-    [ "__NOMAD400_DISPLAYATTRIBUTESDEFINITION__", "display_attributes_definition_8hpp.html#a3e289269583f8e4e09dd798752d3d32c", null ],
     [ "_definition", "display_attributes_definition_8hpp.html#a68ca731e70b4c5ef6ca341d4fa668775", null ]
 ];
