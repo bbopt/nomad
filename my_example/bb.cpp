34f628bd766f1f4eb35c40a8d6dfa31dc2637388
Renaud Saltet <saltrena@zeta.gerad.lan>
2021-02-08 09:16:10 -0500

    ajout du dossier my_example

---
+++ my_example/bb.cpp
@@ -0,0 +1,134 @@
+/*---------------------------------------------------------------------------------*/
+/*  NOMAD - Nonlinear Optimization by Mesh Adaptive Direct Search -                */
+/*                                                                                 */
+/*  NOMAD - Version 4.0.0 has been created by                                      */
+/*                 Viviane Rochon Montplaisir  - Polytechnique Montreal            */
+/*                 Christophe Tribes           - Polytechnique Montreal            */
+/*                                                                                 */
+/*  The copyright of NOMAD - version 4.0.0 is owned by                             */
+/*                 Charles Audet               - Polytechnique Montreal            */
+/*                 Sebastien Le Digabel        - Polytechnique Montreal            */
+/*                 Viviane Rochon Montplaisir  - Polytechnique Montreal            */
+/*                 Christophe Tribes           - Polytechnique Montreal            */
+/*                                                                                 */
+/*  NOMAD v4 has been funded by Rio Tinto, Hydro-Québec, NSERC (Natural            */
+/*  Sciences and Engineering Research Council of Canada), InnovÉÉ (Innovation      */
+/*  en Énergie Électrique) and IVADO (The Institute for Data Valorization)         */
+/*                                                                                 */
+/*  NOMAD v3 was created and developed by Charles Audet, Sebastien Le Digabel,     */
+/*  Christophe Tribes and Viviane Rochon Montplaisir and was funded by AFOSR       */
+/*  and Exxon Mobil.                                                               */
+/*                                                                                 */
+/*  NOMAD v1 and v2 were created and developed by Mark Abramson, Charles Audet,    */
+/*  Gilles Couture, and John E. Dennis Jr., and were funded by AFOSR and           */
+/*  Exxon Mobil.                                                                   */
+/*                                                                                 */
+/*  Contact information:                                                           */
+/*    Polytechnique Montreal - GERAD                                               */
+/*    C.P. 6079, Succ. Centre-ville, Montreal (Quebec) H3C 3A7 Canada              */
+/*    e-mail: nomad@gerad.ca                                                       */
+/*                                                                                 */
+/*  This program is free software: you can redistribute it and/or modify it        */
+/*  under the terms of the GNU Lesser General Public License as published by       */
+/*  the Free Software Foundation, either version 3 of the License, or (at your     */
+/*  option) any later version.                                                     */
+/*                                                                                 */
+/*  This program is distributed in the hope that it will be useful, but WITHOUT    */
+/*  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or          */
+/*  FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License    */
+/*  for more details.                                                              */
+/*                                                                                 */
+/*  You should have received a copy of the GNU Lesser General Public License       */
+/*  along with this program. If not, see <http://www.gnu.org/licenses/>.           */
+/*                                                                                 */
+/*  You can find information on the NOMAD software at www.gerad.ca/nomad           */
+/*---------------------------------------------------------------------------------*/
+#include <fstream>      // For ifstream
+#include <iostream>
+#include <cmath>        // For sqrt
+#include <stdexcept>    // For logic_error
+
+const int n = 10;
+
+int main (int argc, char **argv)
+{
+    bool eval_ok = false;
+
+    // Remotely based on G2.
+    double f = 1e+20, g1 = 1e+20, g2 = 1e+20, g3 = 1e+20;
+    double sum1 = 0.0, sum2 = 0.0, sum3 = 0.0, prod1 = 1.0, prod2 = 1.0;
+    double x[n];
+
+    bool x0read = false;
+    if (argc >= 2)
+    {
+        std::string x0file = argv[1];
+        std::ifstream in (argv[1]);
+        for (int i = 0; i < n; i++)
+        {
+            if (in.fail())
+            {
+                std::cerr << "Error reading file " << x0file << " for x0." << std::endl;
+                x0read = false;
+                break;
+            }
+            in >> x[i];
+            x0read = true;
+        }
+        in.close();
+    }
+
+    if (x0read)
+    {
+        try
+        {
+            for (int i = 0; i < n ; i++)
+            {
+                sum1  += pow(cos(x[i]), 4);
+                sum2  += x[i];
+                sum3  += (i+1)*x[i]*x[i];
+                prod1 *= pow(cos(x[i]), 2);
+                if (prod2 != 0.0)
+                {
+                    if (x[i] == 0.0)
+                    {
+                        prod2 = 0.0;
+                    }
+                    else
+                    {
+                        prod2 *= x[i];
+                    }
+                }
+            }
+
+            g1 = -prod2 + 0.75;
+            g2 = sum2 -7.5 * n;
+
+            f = 10*g1 + 10*g2;
+            if (0.0 != sum3)
+            {
+                f -= (sum1 -2*prod1) / std::abs(sqrt(sum3));
+            }
+            // Scale
+            if (!std::isnan(f))
+            {
+                f *= 1e-5;
+            }
+
+            eval_ok = !std::isnan(f);
+
+            g3 = - (f + 2000);
+        }
+        catch (std::exception &e)
+        {
+            std::string err("Exception: ");
+            err += e.what();
+            throw std::logic_error(err);
+        }
+    }
+
+    std::cout << f << " " << g1 << " " << g2 << " " << g3 << std::endl;
+
+    // Return 0 if eval_ok.
+    return !eval_ok;
+}
