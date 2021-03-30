869dbbcd44fb41c1fc373108ee7920eea623d080
Viviane Rochon Montplaisir <viviane.rochon.montplaisir@gerad.ca>
2021-03-30 16:33:59 -0400

    add missing files

---
+++ src/Param/DeprecatedParameters.hpp
@@ -0,0 +1,82 @@
+/*---------------------------------------------------------------------------------*/
+/*  NOMAD - Nonlinear Optimization by Mesh Adaptive Direct Search -                */
+/*                                                                                 */
+/*  NOMAD - Version 4.0.0 has been created by                                      */
+/*                 Viviane Rochon Montplaisir  - Polytechnique Montreal            */
+/*                 Christophe Tribes           - Polytechnique Montreal            */
+/*                                                                                 */
+/*  The copyright of NOMAD - version 4.0.0 is owned by                             */
+/*                 Charles Audet               - Polytechnique Montreal            */
+/*                 Sebastien Le Digabel        - Polytechnique Montreal            */
+/*                 Viviane Rochon Montplaisir  - Polytechnique Montreal            */
+/*                 Christophe Tribes           - Polytechnique Montreal            */
+/*                                                                                 */
+/*  NOMAD v4 has been funded by Rio Tinto, Hydro-Québec, NSERC (Natural            */
+/*  Sciences and Engineering Research Council of Canada), InnovÉÉ (Innovation      */
+/*  en Énergie Électrique) and IVADO (The Institute for Data Valorization)         */
+/*                                                                                 */
+/*  NOMAD v3 was created and developed by Charles Audet, Sebastien Le Digabel,     */
+/*  Christophe Tribes and Viviane Rochon Montplaisir and was funded by AFOSR       */
+/*  and Exxon Mobil.                                                               */
+/*                                                                                 */
+/*  NOMAD v1 and v2 were created and developed by Mark Abramson, Charles Audet,    */
+/*  Gilles Couture, and John E. Dennis Jr., and were funded by AFOSR and           */
+/*  Exxon Mobil.                                                                   */
+/*                                                                                 */
+/*  Contact information:                                                           */
+/*    Polytechnique Montreal - GERAD                                               */
+/*    C.P. 6079, Succ. Centre-ville, Montreal (Quebec) H3C 3A7 Canada              */
+/*    e-mail: nomad@gerad.ca                                                       */
+/*                                                                                 */
+/*  This program is free software: you can redistribute it and/or modify it        */
+/*  under the terms of the GNU Lesser General Public License as published by       */
+/*  the Free Software Foundation, either version 3 of the License, or (at your     */
+/*  option) any later version.                                                     */
+/*                                                                                 */
+/*  This program is distributed in the hope that it will be useful, but WITHOUT    */
+/*  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or          */
+/*  FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License    */
+/*  for more details.                                                              */
+/*                                                                                 */
+/*  You should have received a copy of the GNU Lesser General Public License       */
+/*  along with this program. If not, see <http://www.gnu.org/licenses/>.           */
+/*                                                                                 */
+/*  You can find information on the NOMAD software at www.gerad.ca/nomad           */
+/*---------------------------------------------------------------------------------*/
+#ifndef __NOMAD400_DEPRECATEDPARAMETERS__
+#define __NOMAD400_DEPRECATEDPARAMETERS__
+
+#include "../Param/Parameters.hpp"
+
+#include "../nomad_nsbegin.hpp"
+
+/// The class for deprecated parameters.
+/**
+ - Register all deprecated parameters during construction.
+ - Trigger exception if a deprecated parameter has been explicitly set.
+*/
+class DeprecatedParameters final : public Parameters
+{
+public:
+    /// Constructor
+    explicit DeprecatedParameters()
+    : Parameters()
+    {
+        init();
+    }
+    /// Read and detect explicity set deprecated parameter. Trigger exception.
+    void readAndDetectExplicitlySet( );
+
+private:
+    /// Helper for constructor
+    /**
+     Register and set default values for all deprecated attributes. The information to register all the attributes is contained in deprecatedAttributesDefinition.hpp as a set of strings to be interpreted. This file is created by the writeAttributeDefinition executable from the deprecatedAttributeDefinition.txt.
+     */
+    void init() override ;
+
+};
+
+#include "../nomad_nsend.hpp"
+
+#endif // __NOMAD400_DEPRECATEDPARAMETERS__
+
