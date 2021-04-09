2c14f9d0956a119c7e3479d3012020805cc9da09
Viviane Rochon Montplaisir <viviane.rochon.montplaisir@gerad.ca>
2021-04-09 13:39:01 -0400

    Update doxygen doc

--- doc/doxygen/html/class_compute_success_type.js
+++ doc/doxygen/html/class_compute_success_type.js
@@ -1,8 +1,8 @@
 var class_compute_success_type =
 [
     [ "ComputeSuccessType", "class_compute_success_type.html#ac64b28a892a989d405d8bbed3d00d56b", null ],
+    [ "computeSuccessTypeModel", "class_compute_success_type.html#a9330212be581e9867d08b2b95bfe9435", null ],
     [ "computeSuccessTypePhaseOne", "class_compute_success_type.html#a5f555748e53b01c94dde5c27f8b0c43e", null ],
-    [ "computeSuccessTypeSgte", "class_compute_success_type.html#a4ee1bbd8d1699a7f4aaeeff6e881cedd", null ],
     [ "defaultComputeSuccessType", "class_compute_success_type.html#a68cfd2c7b1ccb22dc46fe152505f0044", null ],
     [ "operator()", "class_compute_success_type.html#ab0a4b25f3f723f8120b3e4230ddcab7a", null ],
     [ "setComputeSuccessTypeFunction", "class_compute_success_type.html#adb02b8f294e6e8113aa5c6053a02c2fe", null ],
