c818775f03406baa022647d5d1de9f063de970dc
Viviane Rochon Montplaisir <viviane.rochon.montplaisir@gerad.ca>
2021-04-14 13:31:30 -0400

    update script for csv table

--- doc/user_guide/bin/getAllParametersInfo.sh
+++ doc/user_guide/bin/getAllParametersInfo.sh
@@ -3,5 +3,5 @@
 # Show txt files except for deprecated parameters;
 # Extract informations in CSV format;
 # Sort (by parameter name).
-ls *.txt | grep -v deprecated | xargs cat | extractParamInfo.sh | sort
+ls $NOMAD_HOME/src/Attribute/*.txt | grep -v deprecated | xargs cat | extractParamInfo.sh | sort > $NOMAD_HOME/doc/user_guide/source/allParameters.csv
 
