f597e3a5690dd4dba90ee4ca26820befff4d3ca2
Christophe Tribes <christophe.tribes@polymtl.ca>
2022-02-21 13:37:27 -0500

    Update from private nomad4dev

--- interfaces/Matlab_MEX/Functions/nomadOpt.m
+++ interfaces/Matlab_MEX/Functions/nomadOpt.m
@@ -4,15 +4,16 @@
 %    x                            lb <= x <= ub
 %                                 x in R
 %            
-%%%%%%%%%%%%% GERAD VERSION %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%   [x,fval,exitflag,iter,nfval] = nomadOpt(fun,x0,lb,ub,params)
+%%%%%%%%%%%%% GERAD VERSION %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%   [x,fval,exitflag,iter,nfval] = nomadOpt(fun,x0,lb,ub,params,cbFun)
 %
 %   Input arguments:
 %       fun - nonlinear function handle (f and nlcon)
-%       x0 - initial solution guess
-%       lb - decision variable lower bounds
-%       ub - decision variable upper bounds
-%       params - parameters passed to the blackbox function as an array of strings
+%       x0 - initial solution guess.
+%       lb - decision variable lower bounds. An empty lb ([]) can be provided.
+%       ub - decision variable upper bounds. An empty ub ([]) can be provided.
+%       params - parameters passed to the blackbox function as an array of strings using Nomad syntax (see user guide for the syntax).
+%       cbFun - callback function handle (optional). When present, the function is called after calling fun. The user must provide a function that receives three input arguments: count_eval, fevals and x. Count_eval is the number of times fun has been called. Fevals and x are respectively the outputs returned by fun and its inputs. The function returns a boolean. If cbFun returns true, the optimization stops prematurely.
 %
 %   Return arguments:
 %       x - solution vector
