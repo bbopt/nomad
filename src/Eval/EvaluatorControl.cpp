fcd2563e6d5b7da45f2b97dc4ee59f4b0182c45c
Sobhan Mohammadpour <www.sobhan.mohammadpour@gmail.com>
2022-07-19 15:21:34 -0400

    Update EvaluatorControl.cpp

--- src/Eval/EvaluatorControl.cpp
+++ src/Eval/EvaluatorControl.cpp
@@ -1187,7 +1187,7 @@ NOMAD::SuccessType NOMAD::EvaluatorControl::run()
                 warningShown = true;
             }
             OUTPUT_INFO_END
-            usleep(10);
+            usleep(10u);
 
             // Update stopReason in case we found a success
             if (getOpportunisticEval(threadNum)
