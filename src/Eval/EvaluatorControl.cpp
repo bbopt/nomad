1cbaadf0024039306118f525a5ca9f977b2843ec
Christophe Tribes <christophe.tribes@polymtl.ca>
2022-07-21 16:30:22 -0400

    Force use of my_usleep

--- src/Eval/EvaluatorControl.cpp
+++ src/Eval/EvaluatorControl.cpp
@@ -1187,7 +1187,7 @@ NOMAD::SuccessType NOMAD::EvaluatorControl::run()
                 warningShown = true;
             }
             OUTPUT_INFO_END
-            usleep(10);
+            my_usleep(10);
 
             // Update stopReason in case we found a success
             if (getOpportunisticEval(threadNum)
