aca72af7ccaf10321d8e8a151bff9cff89079d5a
Jan Provaznik <jan@provaznik.pro>
2023-01-12 19:35:08 +0100

    Merged static libraries into nomadStatic.
    Overall,
    - added PyNomadStatic cmake taget,
    - improved PyNomadStatic setup and added proper support for MSVC.

--- interfaces/PyNomad/setup_PyNomadStatic.py
+++ interfaces/PyNomad/setup_PyNomadStatic.py
@@ -8,6 +8,7 @@
 #
 # (1) NOMAD_INSTALL for NOMAD installation directory.
 # (2) NOMAD_VERSION for NOMAD version.
+# (3) NOMAD_VIAMSVC for signalization that MSVC will be used.
 #
 # Export the variables. 
 
@@ -18,38 +19,62 @@ import sys
 import os.path
 import os
 
-nomad_install = os.environ['NOMAD_INSTALL']
-nomad_version = os.environ['NOMAD_VERSION']
+# Environment processing
 
-if not(nomad_install):
+env_nomad_install = os.environ.get('NOMAD_INSTALL')
+env_nomad_version = os.environ.get('NOMAD_VERSION')
+env_nomad_viamsvc = os.environ.get('NOMAD_VIAMSVC')
+
+if not(env_nomad_install):
     print('Missing NOMAD_INSTALL env.')
     sys.exit(1)
 
-if not(nomad_version):
+if not(env_nomad_version):
     print('Missing NOMAD_VERSION env')
     sys.exit(1)
 
-# Current support is limited to Linux (with GCC) only.
+if not(env_nomad_viamsvc):
+    print('Missing NOMAD_VIAMSVC, assuming GCC compatible compiler.')
 
-compile_args = []
-compile_args.append('-w')
-compile_args.append('-std=c++14')
-compile_args.append('-pthread')
+# Construct paths
 
-nomad_include_path = os.path.join(nomad_install, 'include/nomad')
-nomad_library_path = os.path.join(nomad_install, 'lib')
+nomad_include_path = os.path.join(env_nomad_install, 'include', 'nomad')
+nomad_library_path = os.path.join(env_nomad_install, 'lib')
 
-link_args = []
-link_args.append('-L{}'.format(nomad_library_path))
-link_args.append('-l:libnomadAlgosStatic.a')
-link_args.append('-l:libnomadEvalStatic.a')
-link_args.append('-l:libnomadUtilsStatic.a')
-link_args.append('-l:libsgtelibStatic.a')
-link_args.append('-lgomp')
+nomad_library_list = [ 
+    'nomadStatic', 
+    'sgtelibStatic' 
+]
+
+# Compiler and linker configuration
+
+setup_compile_args = []
+setup_compile_args.append('-DNOMAD_STATIC_BUILD')
+
+if env_nomad_viamsvc:
+    setup_compile_args.append('/std:c++14')
+else:
+    setup_compile_args.append('-w')
+    setup_compile_args.append('-std=c++14')
+    setup_compile_args.append('-pthread')
+
+setup_libraries = []
+setup_library_dirs = []
+setup_extra_objects = []
+
+if env_nomad_viamsvc:
+    setup_libraries.extend(nomad_library_list)
+    setup_library_dirs.append(nomad_library_path)
+else:
+    for library_name in nomad_library_list:
+        library_path = os.path.join(nomad_library_path, f'lib{library_name}.a') 
+        setup_extra_objects.append(library_path)
+
+# Off we go.
 
 setuptools.setup(
     name = 'PyNomad',
-    version = nomad_version,
+    version = env_nomad_version,
     author = 'Christophe Tribes',
     author_email = 'christophe.tribes@polymtl.ca',
     license = 'LGPL',
@@ -57,14 +82,13 @@ setuptools.setup(
     url = 'https://github.com/bbot/nomad',
     ext_modules = cythonize(setuptools.Extension(
         'PyNomad',
-        sources  =  [ 'PyNomad.pyx' ],
-        include_dirs  =  [ nomad_include_path ],
-        extra_compile_args  =  compile_args,
-        extra_link_args  =  link_args,
+        sources = [ 'PyNomad.pyx' ],
+        include_dirs = [ nomad_include_path ],
+        extra_compile_args = setup_compile_args,
+        extra_objects = setup_extra_objects,
+        libraries = setup_libraries,
+        library_dirs = setup_library_dirs,
         language  =  'c++'
     )),
-    install_requires = [
-        'Cython'
-    ]
 )
 
