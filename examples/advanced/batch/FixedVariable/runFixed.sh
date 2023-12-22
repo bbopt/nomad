303386939da1e9d0953cf1fffa3c212a827484d4
Christophe Tribes <christophe.tribes@polymtl.ca>
2023-12-22 10:39:32 -0500

    Update to make test on fixed variable more adapted to github check tests

--- examples/advanced/batch/FixedVariable/runFixed.sh
+++ examples/advanced/batch/FixedVariable/runFixed.sh
@@ -1,19 +1,21 @@
 #Batch executable to run a problem using a series of fixed values.
 
+# Argument #1: path to Nomad install dir
+
 rm -f cache.txt
 # Fix 0-2
 echo "Fix 0-2"
-../../../../bin/nomad param1.txt
+$1/bin/nomad param1.txt
 echo "Cache size: "; wc -l cache.txt
 # Fix 2-3
 echo "Fix 2-3"
-../../../../bin/nomad param2.txt
+$1/bin/nomad param2.txt
 echo "Cache size: "; wc -l cache.txt
 # Fix 3-4
 echo "Fix 3-4"
-../../../../bin/nomad param3.txt
+$1/bin/nomad param3.txt
 echo "Cache size: "; wc -l cache.txt
 # Fix nothing
 echo "Fix nothing"
-../../../../bin/nomad param10.txt
+$1/bin/nomad param10.txt
 echo "Cache size: "; wc -l cache.txt
--- examples/advanced/batch/FixedVariable/runFixedWin.sh
+++ examples/advanced/batch/FixedVariable/runFixedWin.sh
@@ -1,19 +1,21 @@
 #Batch executable to run a problem using a series of fixed values.
 
+# Argument #1: path to Nomad install dir
+
 rm -f cache.txt
 # Fix 0-2
 echo "Fix 0-2"
-nomad.exe param1.txt
+%1\bin\nomad.exe param1.txt
 echo "Cache size: "; wc -l cache.txt
 # Fix 2-3
 echo "Fix 2-3"
-nomad.exe param2.txt
+%1\bin\nomad.exe param2.txt
 echo "Cache size: "; wc -l cache.txt
 # Fix 3-4
 echo "Fix 3-4"
-nomad.exe param3.txt
+%1\bin\nomad.exe param3.txt
 echo "Cache size: "; wc -l cache.txt
 # Fix nothing
 echo "Fix nothing"
-nomad.exe param10.txt
+%1\bin\nomad.exe param10.txt
 echo "Cache size: "; wc -l cache.txt
