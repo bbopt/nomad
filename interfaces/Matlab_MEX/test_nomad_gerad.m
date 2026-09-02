f8ff23b1f4e03542393e3a07d0c8246e1612cce9
Christophe Tribes <christophe.tribes@polymtl.ca>
2026-09-02 12:57:40 -0400

    Update test_nomad_gerad.m

--- interfaces/Matlab_MEX/test_nomad_gerad.m
+++ interfaces/Matlab_MEX/test_nomad_gerad.m
@@ -19,8 +19,7 @@ params = struct('initial_mesh_size','* 10','MAX_BB_EVAL','100');
 % %% PROBLEM 2
 % % Blackbox Function
 % %clc
-% bb = @(x) [29.4*x(1) + 18*x(2);
-%            -(x(1) - 0.2458*x(1)^2/x(2)) + 6];
+% bb = @(x) [29.4*x(1) + 18*x(2),-(x(1) - 0.2458*x(1)^2/x(2)) + 6];
 % % Bounds      
 % lb = [0;1e-5];
 % ub = [115.8;30];
@@ -46,10 +45,7 @@ params = struct('initial_mesh_size','* 10','MAX_BB_EVAL','100');
 % 
 % PROBLEM 4 [fval = -2.5]
 % %clc
-% fun = @(x)   [-x(1) - x(2) - x(3);
-%               (x(2) - 1./2.)*(x(2) - 1./2.) + (x(3) - 1./2.)*(x(3) - 1./2.) - 1/4;
-%                 x(1) - x(2);
-%                 x(1) + x(3) + x(4) - 2];          
+% fun = @(x)   [-x(1) - x(2) - x(3),(x(2) - 1./2.)*(x(2) - 1./2.) + (x(3) - 1./2.)*(x(3) - 1./2.) - 1/4, x(1) - x(2),x(1) + x(3) + x(4) - 2];
 % ub = [1;10;10;5];
 % lb = [0;0;0;0];
 % x0 = [0;0;0;0];
@@ -71,8 +67,7 @@ params = struct('initial_mesh_size','* 10','MAX_BB_EVAL','100');
 %% St_e01 [x = 6,0.6667, fval = -6.6667]
 % % Blackbox Function
 % %clc
-% bb = @(x) [-x(1) - x(2);
-%            x(1)*x(2) - 4];
+% bb = @(x) [-x(1) - x(2),x(1)*x(2) - 4];
 % % Bounds
 % lb = [0;0];
 % ub = [6;4];
@@ -86,9 +81,7 @@ params = struct('initial_mesh_size','* 10','MAX_BB_EVAL','100');
 % %% St_e08 [x = 0.1294, 0.4830, fval = 0.7418]
 % %clc
 % % Blackbox Function
-% bb = @(x) [2*x(1)+x(2);
-%            -16*x(1)*x(2) + 1; 
-%            (-4*x(1)^2) - 4*x(2)^2 + 1];
+% bb = @(x) [2*x(1)+x(2),-16*x(1)*x(2) + 1,(-4*x(1)^2) - 4*x(2)^2 + 1];
 % % Bounds       
 % lb = [0;0];
 % ub = [1;1];
@@ -102,8 +95,7 @@ params = struct('initial_mesh_size','* 10','MAX_BB_EVAL','100');
 %% Wolfram problem (multiple global minima fval = 0)
 % %clc
 % % Fitting Function
-% obj = @(x) [x(1) - sin(2*x(1) + 3*x(2)) - cos(3*x(1) - 5*x(2));
-%           x(2) - sin(x(1) - 2*x(2)) + cos(x(1) + 3*x(2))];      
+% obj = @(x) [x(1) - sin(2*x(1) + 3*x(2)) - cos(3*x(1) - 5*x(2)), x(2) - sin(x(1) - 2*x(2)) + cos(x(1) + 3*x(2))];
 % % Blackbox Function      
 % bb = @(x) norm(obj(x));
 % 
@@ -119,8 +111,7 @@ params = struct('initial_mesh_size','* 10','MAX_BB_EVAL','100');
 % 
 % %% MINLP 1 [fval = -5]
 % clc
-% fun = @(x) [ (x(1) - 5)^2 + x(2)^2 - 25;
-%               x(1)^2 - x(2) + 0.5 ];
+% fun = @(x) [ (x(1) - 5)^2 + x(2)^2 - 25,x(1)^2 - x(2) + 0.5 ];
 % x0 = [0;0];
 % params = struct('display_degree','2','bb_input_type','(I I)','bb_output_type','OBJ PB','max_bb_eval','20');
 % [xr,fval,ef,iter] = nomadOpt(fun,x0,[],[],params)
